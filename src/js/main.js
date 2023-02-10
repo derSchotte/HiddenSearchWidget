7d93b7c626021ed2723f39b4affa221972882fe1
Frank McDonald <kontakt@frank-mcdonald.de>
2023-02-10 08:18:24 +0100

    add comments to js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 import './../scss/style.scss';
 
+// Fügt den Such-Button zur Seite hinzu
 const search = document.querySelector('.search');
+
+// Deklariert das Suche-Button Element
 const btn = document.querySelector('.btn');
+
+// Deklariert das Input-Element 
 const input = document.querySelector('.input');
 
+// Wenn der Button gedrückt wird, fügt die Klasse "active" hinzu und richtet den Fokus auf das Input-Element
 btn.addEventListener('click', () => {
     search.classList.toggle('active');
     input.focus();
